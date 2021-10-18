ff86bc2e09f33a7d748f2f58158902e164bdead5
weaverHope <92676579+weaverHope@users.noreply.github.com>
2021-10-18 13:35:41 +0200

    Create Iteration#1.js

---
+++ Iteration#1.js
@@ -0,0 +1,9 @@
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+function max(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+      return numberOne;
+  }
+  else if (numberTwo > numberOne) {
+      return numberTwo;
+  }
+}
